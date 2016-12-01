33deb8bf236591bce961209eaad8b81797a072b4
bkraft <bkraft4257@gmail.com>
2016-12-01 17:49:30 -0500

    Added other directory with first draft of redcap_link.bash

---
+++ other/redcap_link.bash
@@ -0,0 +1,7 @@
+
+export TIC_REDCAP_LINK_PATH='/Users/bkraft/PycharmProjects/tic_redcap_link'  # Add path information here
+export TIC_REDCAP_LINK_PYTHONPATH=${TIC_REDCAP_LINK_PATH}/redcap_link
+
+export PYTHONPATH=${TIC_REDCAP_LINK_PYTHONPATH}:$PYTHONPATH
+
+alias  redcap_upload='python3 ${TIC_REDCAP_LINK_PYTHONPATH}/redcap_upload.py' 
