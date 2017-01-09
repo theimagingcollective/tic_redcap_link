b83104b77ba7b4ccd6c13708f0a2631c8f87485b
bkraft <bkraft4257@gmail.com>
2017-01-09 13:12:49 -0500

    Added bash_setup to root directory.

---
+++ tic_redcap_link_bash_setup.sh
@@ -0,0 +1,8 @@
+#
+# TIC RedCap Link Setup
+#
+
+export TIC_REDCAP_LINK_PATH=${TIC_PATH}/tic_redcap_link/
+
+export TIC_MODULES=${TIC_REDCAP_LINK_PATH}/redcap:${TIC_MODULES}
+export PYTHONPATH=${TIC_REDCAP_LINK_PATH}/redcap:$PYTHONPATH
