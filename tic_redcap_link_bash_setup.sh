0119eedc35db0af614d4de9636b4d7e01bec3877
bkraft <bkraft4257@gmail.com>
2017-01-09 15:40:18 -0500

    Fixed bug. alias file was incorrect.

--- tic_redcap_link_bash_setup.sh
+++ tic_redcap_link_bash_setup.sh
@@ -8,4 +8,4 @@ export TIC_REDCAP_LINK_PATH=${TIC_PATH}/tic_redcap_link/
 export TIC_MODULES=${TIC_REDCAP_LINK_PATH}/redcap:${TIC_MODULES}
 export PYTHONPATH=${TIC_REDCAP_LINK_PATH}/redcap:$PYTHONPATH
 
-source $TIC_REDCAP_LINK_PATH/other/tic_redcap_aliases.sh
+source $TIC_REDCAP_LINK_PATH/other/tic_redcap_link_aliases.sh
