6d68d33a3e355aa50f6785f0b190d67a7cb686ac
bkraft <bkraft4257@gmail.com>
2017-01-09 15:37:24 -0500

    Added bash_setup to root directory.

---
+++ other/tic_redcap_link_aliases.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+alias  redcap_upload='python3 ${TIC_REDCAP_LINK_PYTHONPATH}/redcap_upload.py' 
--- tic_redcap_link_bash_setup.sh
+++ tic_redcap_link_bash_setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # TIC RedCap Link Setup
 #
@@ -6,3 +7,5 @@ export TIC_REDCAP_LINK_PATH=${TIC_PATH}/tic_redcap_link/
 
 export TIC_MODULES=${TIC_REDCAP_LINK_PATH}/redcap:${TIC_MODULES}
 export PYTHONPATH=${TIC_REDCAP_LINK_PATH}/redcap:$PYTHONPATH
+
+source $TIC_REDCAP_LINK_PATH/other/tic_redcap_aliases.sh
